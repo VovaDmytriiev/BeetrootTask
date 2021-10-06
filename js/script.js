d6690726c71fd645f9272fdf0a5a24cf29af0000
DVolodymyr <78636394+DVolodymyr@users.noreply.github.com>
2021-10-06 14:26:28 +0300

    add adaptive and changes in menu, header, burger, footer, main, style

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 $("document").ready(function () {
   $(".burger").click(function (event) {
     $(".burger, .menu__list").toggleClass("active");
+    $("body").toggleClass("lock-scroll");
   });
 });
 //============================================================================================
